7048f3ccac92f18ea3e84d1ddf02a5eb2a417fce
NUCAL <nusec_dev_beritanbasoglutest@yopmail.com>
2023-04-14 18:40:08 +0300

    NUCAL-UI | Front-end application initial commit, powered by NUCAL development team.

---
+++ src/components/pages/Screen1.vue
@@ -0,0 +1,85 @@
+<template>
+  <DropZone ncBgType="bgColor" backgroundImage="" id="59112fb8-837a-48aa-ba7e-0fa84516c842"
+    v-bind:style="{'width':'100%','height':'auto'}">
+    <WButtonAvatar src="" href="" icon="" size="" text="" v-bind:badge="false" v-bind:button="true"
+      v-bind:square="false" v-bind:rounded="false" variant="secondary" v-bind:badgeTop="false" v-bind:disabled="false"
+      v-bind:badgeLeft="false" badgeText="" buttonText="Button Text Avatar" linkTarget="_top" ncLinkType="link"
+      v-bind:aspectRatio="true" badgeOffset="" dataLoading="static" badgeVariant="primary" v-bind:hasDesignMode="true"
+      ncTooltipText=""
+      v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':true,'structure':[{'update':{'type':'dataModelForDynamicType'},'propName':'src','displayName':'Value'}]}"
+      toForRouterLink="to" dataModelForDynamicType="string" position="relative"
+      id="3430af53-eb09-4120-a636-e92e5f56d4a7"
+      v-bind:style="{'width':'100%','height':'100%','boxShadow':'','marginTop':'0px','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px','zIndex':2}"/>
+  </DropZone>
+</template>
+<script>
+export default {
+  name: 'Screen1',
+  data() {
+    return {};
+  },
+  props: {},
+  created() {},
+  mounted() {},
+  computed: {},
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@media (min-width: 1024.05px){[id='59112fb8-837a-48aa-ba7e-0fa84516c842'] {
+             display: grid; 
+             grid-template-columns: 34.169921875rem 11.328125rem 54.501953125rem;
+             grid-template-rows: minmax(25.91796875rem, max-content) minmax(3.125rem, max-content) minmax(53.76953125rem, max-content);
+          }
+[id='3430af53-eb09-4120-a636-e92e5f56d4a7']{
+          grid-area: 2/2/3/3;
+          
+          aspect-ratio: 3.625;
+          }}
+
+@media (min-width: 768.05px) and (max-width: 1024px){[id='59112fb8-837a-48aa-ba7e-0fa84516c842'] {
+             display: grid; 
+             grid-template-columns: 42.919921875rem 14.16015625rem 42.919921875rem;
+             grid-template-rows: minmax(32.3974609375rem, max-content) minmax(3.90625rem, max-content) minmax(3.125rem, max-content);
+          }
+[id='3430af53-eb09-4120-a636-e92e5f56d4a7']{
+          grid-area: 2/2/3/3;
+          
+          aspect-ratio: 3.625;
+          }}
+
+@media (min-width: 480.05px) and (max-width: 768px){[id='59112fb8-837a-48aa-ba7e-0fa84516c842'] {
+             display: grid; 
+             grid-template-columns: 40.559895833333336rem 18.880208333333332rem 40.559895833333336rem;
+             grid-template-rows: minmax(43.196614583333336rem, max-content) minmax(5.208333333333333rem, max-content) minmax(4.166666667rem, max-content);
+          }
+[id='3430af53-eb09-4120-a636-e92e5f56d4a7']{
+          grid-area: 2/2/3/3;
+          
+          aspect-ratio: 3.625;
+          }}
+
+@media (min-width: 375.05px) and (max-width: 480px){[id='59112fb8-837a-48aa-ba7e-0fa84516c842'] {
+             display: grid; 
+             grid-template-columns: 34.895833333333336rem 30.208333333333332rem 34.895833333333336rem;
+             grid-template-rows: minmax(69.11458333333333rem, max-content) minmax(8.333333333333334rem, max-content) minmax(5rem, max-content);
+          }
+[id='3430af53-eb09-4120-a636-e92e5f56d4a7']{
+          grid-area: 2/2/3/3;
+          
+          aspect-ratio: 3.625;
+          }}
+
+@media (min-width: 0px) and (max-width: 375px){[id='59112fb8-837a-48aa-ba7e-0fa84516c842'] {
+             display: grid; 
+             grid-template-columns: 6.4rem 87.2rem 6.4rem;
+             grid-template-rows: minmax(88.46666666666667rem, max-content) minmax(24.055172413793105rem, max-content) minmax(6.4rem, max-content);
+          }
+[id='3430af53-eb09-4120-a636-e92e5f56d4a7']{
+          grid-area: 2/2/3/3;
+          
+          aspect-ratio: 3.625;
+          }}
+</style>
